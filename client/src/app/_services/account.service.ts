2634f52408610c2ff395856719fe264b9f7b5626
madbeecreative <emma@madbeecreative.com>
2021-04-04 19:53:41 +1000

    End of section 17

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,20 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 import { User } from '../_models/user';
+import { ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private presence: PresenceService) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
@@ -22,16 +22,18 @@ export class AccountService {
         const user = response;
         if (user) {
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
       })
-    );
+    )
   }
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
-      map((user: User ) => {
+      map((user: User) => {
         if (user) {
-          this.setCurrentUser(user);
+         this.setCurrentUser(user);
+         this.presence.createHubConnection(user);
         }
       })
     )
@@ -48,7 +50,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
-
+    this.presence.stopHubConnection();
   }
 
   getDecodedToken(token) {
